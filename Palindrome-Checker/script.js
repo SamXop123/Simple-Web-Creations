4cce6eb45e7bfcbef19e62d8ba8012b71bb97622
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-04-13 12:59:49 +0530

    Update script.js

--- Palindrome-Checker/script.js
+++ Palindrome-Checker/script.js
@@ -15,3 +15,20 @@ about.addEventListener('click', () => {
     }
 });
 
+btn.addEventListener('click', ()=>{
+    let win = document.querySelector(".win-message")
+    let input = document.querySelector("input"); 
+    let inputValue = input.value;
+
+    if(inputValue==""){
+        win.innerHTML = "Please enter a text!";
+        win.setAttribute('style', 'display: flex; color: darkred;');
+    }
+    else if(inputValue === inputValue.split('').reverse().join('').toLowerCase()) {
+        win.innerHTML = `Yes, ${inputValue} is a palindrome!`;
+        win.setAttribute('style', 'display: flex');
+    } else {
+        win.innerHTML = `No, ${inputValue} is not a palindrome!`;
+        win.setAttribute('style', 'display: flex');
+    }
+})
