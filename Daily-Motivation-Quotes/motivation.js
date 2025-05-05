67b547593c3da74b24da1ae1a0175452b73528d1
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-05-06 01:10:26 +0530

    Update motivation.js

--- Daily-Motivation-Quotes/motivation.js
+++ Daily-Motivation-Quotes/motivation.js
@@ -35,3 +35,18 @@ function displayDate() {
     document.getElementById('currentDate').innerText = today.toLocaleDateString('en-US', options);
 }
 
+function createParticles() {
+    const particlesContainer = document.getElementById('particles');
+    for (let i = 0; i < 50; i++) {
+        const particle = document.createElement('div');
+        particle.classList.add('particle');
+        particle.style.width = `${Math.random() * 5 + 5}px`;
+        particle.style.height = particle.style.width;
+        particle.style.left = `${Math.random() * 100}vw`;
+        particle.style.top = `${Math.random() * 100}vh`;
+        particle.style.animationDuration = `${Math.random() * 10 + 10}s`;
+        particle.style.animationDelay = `${Math.random() * 5}s`;
+        particlesContainer.appendChild(particle);
+    }
+}
+
